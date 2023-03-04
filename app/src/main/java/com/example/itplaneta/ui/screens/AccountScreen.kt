fe4e0a327a9a9c6afbf1e193152d076b625fbea1
Fructuss <afanasev10056@gmail.com>
2023-03-04 14:19:38 +0300

    add topbar with back and save button

--- app/src/main/java/com/example/itplaneta/ui/screens/AccountScreen.kt
+++ app/src/main/java/com/example/itplaneta/ui/screens/AccountScreen.kt
@@ -1,18 +1,24 @@
 package com.example.itplaneta.ui.screens
 
 import android.annotation.SuppressLint
+import android.widget.RadioGroup
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.itplaneta.R
 import com.example.itplaneta.otp.OtpDigest
 import com.example.itplaneta.otp.OtpType
 import com.example.itplaneta.ui.viewmodels.AccountViewModel
@@ -24,47 +30,89 @@ fun AccountScreen(viewModel: AccountViewModel, navController: NavHostController)
     var label by rememberSaveable { mutableStateOf("") }
     var secret by rememberSaveable { mutableStateOf("") }
     var issuer by rememberSaveable { mutableStateOf("") }
+    var selected by rememberSaveable { mutableStateOf("") }
 
-    Surface(
+    Scaffold(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(5.dp)
+            .fillMaxSize(),
+        topBar = {
+            TopAppBar() {
+                IconButton(onClick = { navController.popBackStack() }) {
+                    Icon(Icons.Default.Close, contentDescription = "Назад")
+                }
+
+                Spacer(Modifier.weight(1f, true))
+
+                TextButton(
+                    onClick = {
+                            viewModel.addAccount(
+                                com.example.itplaneta.data.database.Account(
+                                    0,
+                                    label = label,
+                                    issuer = issuer,
+                                    tokenType = OtpType.Totp,
+                                    algorithm = OtpDigest.Sha1,
+                                    secret = secret,
+                                    digits = 6,
+                                    counter = 0,
+                                    period = 30
+                                )
+                            )
+                            navController.popBackStack(route = "main", inclusive = false, saveState = false)
+                    },
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent)) {
+
+                    Text(text = "сохранить", color = Color.White)
+                }
+            }
+        }
     ) {
 
-        Column(modifier = Modifier.fillMaxWidth()) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp, 5.dp)) {
             OutlinedTextField(
-
-                value = issuer, onValueChange = {
+                modifier = Modifier.fillMaxWidth(),
+                value = issuer,
+                onValueChange = {
                     issuer = it
-                }, label = { Text("Платформа аккаунта") })
+                },
+                label = { Text("Платформа аккаунта") }
+            )
             OutlinedTextField(
-
-                value = label, onValueChange = {
+                modifier = Modifier.fillMaxWidth(),
+                value = label,
+                onValueChange = {
                     label = it
-                }, label = { Text("Название аккаунта") })
+                },
+                label = { Text("Название аккаунта") }
+            )
             OutlinedTextField(
-
-                value = secret, onValueChange = {
+                modifier = Modifier.fillMaxWidth(),
+                value = secret,
+                onValueChange = {
                     secret = it
-                }, label = { Text("Код") })
-            OutlinedButton(onClick = {
-                viewModel.addAccount(
-                    com.example.itplaneta.data.database.Account(
-                        0,
-                        label = label,
-                        issuer = issuer,
-                        tokenType = OtpType.Totp,
-                        algorithm = OtpDigest.Sha1,
-                        secret = secret,
-                        digits = 6,
-                        counter = 0,
-                        period = 30
+                },
+                label = { Text("Код") }
+            )
+            val otpList =  stringArrayResource(R.array.test)
+            otpList.forEach { item ->
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(
+                        selected = selected == item,
+                        onClick = {
+
+                        },
+                        enabled = true,
+                        colors = RadioButtonDefaults.colors(
+                            selectedColor = Color.Magenta
+                        )
                     )
-                )
-                navController.popBackStack(route = "main", inclusive = false, saveState = false)
-            }) {
-                Text("Сохранить", fontSize = 25.sp)
+                    Text(text = item, modifier = Modifier.padding(start = 8.dp))
+                }
             }
+
+
         }
     }
 }
