14d2548ae6b751f4c7ba1ddf2c5615485e0f102d
Fructuss <Afanasev10056@gmail.com>
2023-04-03 19:32:26 +0300

    add screens

---
+++ app/src/main/java/com/example/itplaneta/ui/navigation/Screens.kt
@@ -0,0 +1,10 @@
+package com.example.itplaneta.ui.navigation
+
+sealed class Screens(val route  : String){
+    object Main : Screens("main_screen")
+    object AddAccount : Screens("account_screen")
+    object EditAccount : Screens("account_screen/{accountId}"){
+        fun passAccountId(accountId: Long) = "account_screen/$accountId"
+    }
+    object QrScanner : Screens("qrscanner_screen")
+}
