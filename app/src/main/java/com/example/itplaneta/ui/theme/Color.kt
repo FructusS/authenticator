f3c17121dc599329810093baa981abb682f5b89a
Fructuss <Afanasev10056@gmail.com>
2023-04-15 19:57:27 +0300

    add colors for dark and light mode

--- app/src/main/java/com/example/itplaneta/ui/theme/Color.kt
+++ app/src/main/java/com/example/itplaneta/ui/theme/Color.kt
@@ -2,7 +2,20 @@ package com.example.itplaneta.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val backgroundDarkMode = Color(0xFF312E2E)
+val secondaryDarkMode = Color(0xFF605E5E)
+val secondaryVariantDarkMode = Color(0xFFCACACA)
+val primaryDarkMode = Color(0xFF6B6666)
+val primaryVariantDarkMode = Color(0xFF252424)
+val errorDarkMode = Color(0xFF8B0606)
+val surfaceDarkMode = Color(0xFF605E5E)
+
+
+
+val backgroundLightMode = Color.White
+val primaryLightMode = Color(0xFFF1EDED)
+val secondaryLightMode = Color(0xFF605E5E)
+val primaryVariantMode = Color(0xFFE6E6E6)
+val errorLightMode = Color(0xFFAF0808)
+val secondaryVariantLightMode = Color.Black
+val surfaceLightMode = Color(0xFF9B9B9B)
