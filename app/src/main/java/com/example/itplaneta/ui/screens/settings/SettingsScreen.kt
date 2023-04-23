afc90e705bd8616ce2dbe6a57021799d41808ec6
FructusS <afanasev10056@gmail.com>
2023-04-24 00:08:18 +0300

    add radio buttons for change theme

--- app/src/main/java/com/example/itplaneta/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/itplaneta/ui/screens/settings/SettingsScreen.kt
@@ -1,21 +1,25 @@
 package com.example.itplaneta.ui.screens.settings
 
+import android.content.res.Resources.Theme
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.itplaneta.R
 import com.example.itplaneta.ui.navigation.Screens
+import com.example.itplaneta.ui.theme.AuthenticatorTheme
 
 
 @Composable
@@ -42,32 +46,56 @@ fun SettingsScreen(
         },
     ) {
         Column(Modifier.padding(it)) {
-            Card(modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentHeight()
-                .clickable { }) {
-                Text(
-                    text = stringResource(id = R.string.how_it_works),
-                    modifier = Modifier.padding(5.dp, 10.dp)
-                )
-            }
-            Card(modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentHeight()
-                .clickable { }) {
-                Text(
-                    text = stringResource(id = R.string.save_accounts),
-                    modifier = Modifier.padding(5.dp, 10.dp)
-                )
-            }
-            Card(modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentHeight()
-                .clickable { }) {
-                Text(
-                    text = stringResource(id = R.string.load_accounts),
-                    modifier = Modifier.padding(5.dp, 10.dp)
-                )
+            val currentTheme = viewModel.themeState.collectAsState().value
+            val (selected, setSelected) = remember { mutableStateOf(currentTheme) }
+            val themeItems = listOf(AppTheme.Dark,AppTheme.Light,AppTheme.Auto)
+//            Card(modifier = Modifier
+//                .fillMaxWidth()
+//                .wrapContentHeight()
+//                .clickable { viewModel.saveTheme(AppTheme.Light)}) {
+//                Text(
+//                    text = stringResource(id = R.string.how_it_works),
+//                    modifier = Modifier.padding(5.dp, 10.dp)
+//                )
+//            }
+//            Card(modifier = Modifier
+//                .fillMaxWidth()
+//                .wrapContentHeight()
+//                .clickable { viewModel.saveTheme(AppTheme.Dark)}) {
+//                Text(
+//                    text = stringResource(id = R.string.save_accounts),
+//                    modifier = Modifier.padding(5.dp, 10.dp)
+//                )
+//            }
+//            Card(modifier = Modifier
+//                .fillMaxWidth()
+//                .wrapContentHeight()
+//                .clickable {viewModel.saveTheme(AppTheme.Auto) }) {
+//                Text(
+//                    text = stringResource(id = R.string.load_accounts),
+//                    modifier = Modifier.padding(5.dp, 10.dp)
+//                )
+//            }
+            themeItems.forEach { itemTheme ->
+                Row(
+
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = itemTheme.name, modifier = Modifier.padding(start = 8.dp))
+
+                    RadioButton(
+                        selected = selected == itemTheme,
+                        onClick = {
+                            viewModel.saveTheme(itemTheme)
+                        },
+                        enabled = true,
+                        colors = RadioButtonDefaults.colors(
+                            selectedColor = Color.Magenta
+                        )
+                    )
+
+                }
             }
         }
 
