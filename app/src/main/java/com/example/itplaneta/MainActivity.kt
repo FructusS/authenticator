6cf68e8a747f3fba392f417bd23df6fbbf2fe39e
Fructuss <Afanasev10056@gmail.com>
2023-04-23 16:00:47 +0300

    add settings screen route

--- app/src/main/java/com/example/itplaneta/MainActivity.kt
+++ app/src/main/java/com/example/itplaneta/MainActivity.kt
@@ -3,32 +3,35 @@ package com.example.itplaneta
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.itplaneta.ui.navigation.Screens
-import com.example.itplaneta.ui.screens.AccountScreen
-import com.example.itplaneta.ui.screens.MainScreen
-import com.example.itplaneta.ui.screens.ScannerScreen
-import com.example.itplaneta.ui.theme.ItplanetaTheme
-import com.example.itplaneta.ui.viewmodels.QrScannerViewModel
+import com.example.itplaneta.ui.screens.account.AccountScreen
+import com.example.itplaneta.ui.screens.main.MainScreen
+import com.example.itplaneta.ui.screens.qrscanner.ScannerScreen
+import com.example.itplaneta.ui.screens.settings.SettingsScreen
+import com.example.itplaneta.ui.theme.AuthenticatorTheme
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ItplanetaTheme {
+            AuthenticatorTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                 ) {
@@ -38,27 +41,82 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    @OptIn(ExperimentalAnimationApi::class)
     @Composable
     fun MyAppNavHost(
         modifier: Modifier = Modifier,
-        navController: NavHostController = rememberNavController(),
+        navController: NavHostController = rememberAnimatedNavController(),
         startDestination: String = Screens.Main.route
     ) {
-        NavHost(
+        AnimatedNavHost(
             modifier = modifier,
             navController = navController,
             startDestination = startDestination
         ) {
-            composable(Screens.Main.route) {
+            composable(Screens.Main.route, enterTransition = {
+                when (initialState.destination.route) {
+                    Screens.AddAccount.route ->
+                        slideIntoContainer(
+                            AnimatedContentScope.SlideDirection.Right,
+                            animationSpec = tween(350)
+                        )
+                    Screens.Settings.route -> {
+                        slideIntoContainer(
+                            AnimatedContentScope.SlideDirection.Left,
+                            animationSpec = tween(350)
+                        )
+                    }
+                    Screens.EditAccount.route -> {
+                        slideIntoContainer(
+                            AnimatedContentScope.SlideDirection.Right,
+                            animationSpec = tween(350)
+                        )
+                    }
+                    Screens.QrScanner.route -> {
+                        slideIntoContainer(
+                            AnimatedContentScope.SlideDirection.Right,
+                            animationSpec = tween(350)
+                        )
+                    }
+                    else -> null
+                }
+            },
+                exitTransition = {
+                    when (targetState.destination.route) {
+                        Screens.AddAccount.route ->
+                            slideOutOfContainer(
+                                AnimatedContentScope.SlideDirection.Left,
+                                animationSpec = tween(350)
+                            )
+                        Screens.Settings.route ->
+                            slideOutOfContainer(
+                                AnimatedContentScope.SlideDirection.Right,
+                                animationSpec = tween(350)
+                            )
+                        Screens.QrScanner.route ->
+                            slideOutOfContainer(
+                                AnimatedContentScope.SlideDirection.Left,
+                                animationSpec = tween(350)
+                            )
+                        Screens.EditAccount.route ->
+                            slideOutOfContainer(
+                                AnimatedContentScope.SlideDirection.Left,
+                                animationSpec = tween(350)
+                            )
+                        else -> null
+                    }
+                }) {
                 MainScreen(navController = navController)
             }
 
-            composable(route = Screens.AddAccount.route) {
+            composable(
+                route = Screens.AddAccount.route,
+            ) {
                 AccountScreen(navController = navController)
             }
             composable(
                 route = Screens.EditAccount.route,
-                arguments = listOf(navArgument("accountId") { type = NavType.IntType })
+                arguments = listOf(navArgument("accountId") { type = NavType.IntType }),
             ) { backStackEntry ->
                 backStackEntry.arguments?.getInt("accountId")?.let {
                     AccountScreen(
@@ -68,8 +126,13 @@ class MainActivity : ComponentActivity() {
                 }
 
             }
-            composable(route = Screens.QrScanner.route) {
+            composable(
+                route = Screens.QrScanner.route,
+            ) {
                 ScannerScreen(navController)
+            }
+            composable(route = Screens.Settings.route) {
+                SettingsScreen(navController)
             }
         }
     }
