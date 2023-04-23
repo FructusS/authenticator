a456c24d724f8051c862e341a204ff73c41e2624
Fructuss <Afanasev10056@gmail.com>
2023-04-23 16:00:12 +0300

    add settings screen

--- app/src/main/java/com/example/itplaneta/ui/navigation/Screens.kt
+++ app/src/main/java/com/example/itplaneta/ui/navigation/Screens.kt
@@ -3,6 +3,7 @@ package com.example.itplaneta.ui.navigation
 sealed class Screens(val route  : String){
     object Main : Screens("main_screen")
     object AddAccount : Screens("account_screen")
+    object Settings : Screens("settings_screen")
     object EditAccount : Screens("account_screen/{accountId}"){
         fun passAccountId(accountId: Int) = "account_screen/$accountId"
     }
