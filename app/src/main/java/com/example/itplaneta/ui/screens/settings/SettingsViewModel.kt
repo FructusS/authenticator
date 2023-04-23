afd998ef6fe7489d8647af0ad5af45edd7cd92e9
FructusS <afanasev10056@gmail.com>
2023-04-24 00:08:39 +0300

    add save theme

--- app/src/main/java/com/example/itplaneta/ui/screens/settings/SettingsViewModel.kt
+++ app/src/main/java/com/example/itplaneta/ui/screens/settings/SettingsViewModel.kt
@@ -1,9 +1,24 @@
 package com.example.itplaneta.ui.screens.settings
 
+import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SettingsViewModel @Inject constructor() : ViewModel() {
+class SettingsViewModel @Inject constructor(private val settingsManager: SettingsManager) : ViewModel() {
+
+    private val _themeState = MutableStateFlow(settingsManager.getTheme)
+    val themeState = _themeState.asStateFlow()
+    fun saveTheme(theme: AppTheme){
+        viewModelScope.launch {
+            settingsManager.saveTheme(theme)
+        }
+    }
+
+
 }
