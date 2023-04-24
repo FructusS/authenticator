39a83e4cc98d6b1b9fc559101628e8913ca14461
FructusS <afanasev10056@gmail.com>
2023-04-25 00:01:29 +0300

    add main settings item

--- app/src/main/java/com/example/itplaneta/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/itplaneta/ui/screens/settings/SettingsScreen.kt
@@ -11,53 +11,72 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.itplaneta.R
+import com.example.itplaneta.ui.navigation.Screens
 import com.example.itplaneta.ui.screens.component.TopBar
 
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun SettingsScreen(
-    navController: NavHostController,
-    viewModel: SettingsViewModel = hiltViewModel()
+    navController: NavHostController, viewModel: SettingsViewModel = hiltViewModel()
 ) {
-    Scaffold(
-        topBar = {
-            TopBar(navController = navController)
-        }
+    Scaffold(topBar = {
+        TopBar(navController = navController)
+    }
 
     ) {
         Column(
             Modifier
+                .fillMaxSize()
                 .padding(10.dp)
-                .fillMaxWidth()
         ) {
 
             Column(verticalArrangement = Arrangement.spacedBy(5.dp)) {
-
+                Text(
+                    text = stringResource(id = R.string.main_settings),
+                    style = MaterialTheme.typography.h6.copy(fontSize = 16.sp),
+                    color = MaterialTheme.colors.secondary
+                )
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { navController.navigate(Screens.HowItWorks.route)}) {
                     Text(
                         text = stringResource(id = R.string.how_it_works),
-                        modifier = Modifier.padding(5.dp, 10.dp)
+                        modifier = Modifier.padding(0.dp, 10.dp)
+
                     )
+                }
 
 
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { }) {
                     Text(
                         text = stringResource(id = R.string.save_accounts),
-                        modifier = Modifier.padding(5.dp, 10.dp)
+                        modifier = Modifier.padding(0.dp, 10.dp)
                     )
 
+                }
 
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { }) {
                     Text(
                         text = stringResource(id = R.string.load_accounts),
-                        modifier = Modifier.padding(5.dp, 10.dp)
+                        modifier = Modifier.padding(0.dp, 10.dp)
                     )
+                }
+
 
             }
 
-            Divider(color = Color.Black, thickness = 0.4.dp)
-
+            Divider(
+                color = Color.Black, thickness = 0.4.dp, modifier = Modifier.padding(0.dp, 15.dp)
+            )
             ThemeOptions(viewModel = viewModel)
         }
 
@@ -68,15 +87,19 @@ fun SettingsScreen(
 fun ThemeOptions(viewModel: SettingsViewModel) {
     val selected = viewModel.themeState.collectAsState(initial = AppTheme.Auto).value
     val themeItems = listOf(AppTheme.Dark, AppTheme.Light, AppTheme.Auto)
-    Text(text = stringResource(id = R.string.theme))
+    Text(
+        text = stringResource(id = R.string.theme),
+        style = MaterialTheme.typography.h6.copy(fontSize = 16.sp),
+        color = MaterialTheme.colors.secondary
+    )
     themeItems.forEach { itemTheme ->
         Row(
+
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier.clickable {
                 viewModel.saveTheme(itemTheme)
-            }
-        ) {
+            }) {
             when (itemTheme) {
                 AppTheme.Auto -> {
                     Text(
@@ -103,16 +126,12 @@ fun ThemeOptions(viewModel: SettingsViewModel) {
 
 
             RadioButton(
-                selected = selected == itemTheme,
-                onClick = {
+                selected = selected == itemTheme, onClick = {
                     viewModel.saveTheme(itemTheme)
-                },
-                enabled = true,
-                colors = RadioButtonDefaults.colors(
+                }, enabled = true, colors = RadioButtonDefaults.colors(
                     selectedColor = Color.Magenta
                 )
             )
-
         }
     }
 }
