423b5caa832d119aa87c4d0f3cc0582c45749ff6
FructusS <afanasev10056@gmail.com>
2023-05-16 07:10:06 +0300

    add save and restore backup

--- app/src/main/java/com/example/itplaneta/ui/screens/settings/SettingsViewModel.kt
+++ app/src/main/java/com/example/itplaneta/ui/screens/settings/SettingsViewModel.kt
@@ -1,24 +1,87 @@
 package com.example.itplaneta.ui.screens.settings
 
-import androidx.compose.runtime.collectAsState
+import android.content.Context
+import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.itplaneta.data.database.Account
+import com.example.itplaneta.data.database.AccountRepository
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import java.io.*
+import java.nio.charset.StandardCharsets
+import java.util.stream.Collectors
 import javax.inject.Inject
 
+
 @HiltViewModel
-class SettingsViewModel @Inject constructor(private val settingsManager: SettingsManager) : ViewModel() {
+class SettingsViewModel @Inject constructor(
+    @ApplicationContext private val context: Context,
+    private val settingsManager: SettingsManager,
+    private val accountRepository: AccountRepository
+) : ViewModel() {
 
     private val _themeState = MutableStateFlow(settingsManager.getTheme)
     val themeState = _themeState.asStateFlow().value
-    fun saveTheme(theme: AppTheme){
+    fun saveTheme(theme: AppTheme) {
         viewModelScope.launch {
             settingsManager.saveTheme(theme)
         }
     }
+
+
+    fun saveBackupToExternal(uri: Uri) {
+        try {
+            try {
+                context.contentResolver.openFileDescriptor(uri, "w")?.use {
+                    FileOutputStream(it.fileDescriptor).use { it ->
+                        it.write(
+                            GsonBuilder().create().toJson(getAll())
+                                .toByteArray()
+                        )
+                    }
+                }
+            } catch (e: FileNotFoundException) {
+                e.printStackTrace()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+        } catch (_: IOException) {
+
+        }
+    }
+
+    fun restoreBackupFromExternal(uri: Uri) {
+
+        val stringBuilder = StringBuilder()
+        context.contentResolver.openInputStream(uri)?.use { inputStream ->
+            BufferedReader(InputStreamReader(inputStream)).use { reader ->
+                var line: String? = reader.readLine()
+                while (line != null) {
+                    stringBuilder.append(line)
+                    line = reader.readLine()
+                }
+            }
+        }
+        val listType = object : TypeToken<List<Account>>() {}.type
+        val accounts = Gson().fromJson<List<Account>>(stringBuilder.toString(),listType)
+
+        if (accounts.isNotEmpty()){
+            accounts.forEach {
+                    account ->
+                accountRepository.addAccount(account)
+            }
+        }
+    }
+
+    private fun getAll() = accountRepository.getAllAccounts()
 
 
 }
