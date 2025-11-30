fbf6906d1fea54dfc972e1d339bb77abffeab33f
Mihkail Afanasev <afanasev10056@gmail.com>
2025-11-30 23:46:00 +0300

    remove Surface

--- app/src/main/java/com/example/itplaneta/MainActivity.kt
+++ app/src/main/java/com/example/itplaneta/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.example.itplaneta
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.*
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.example.itplaneta.ui.screens.settings.AppTheme
@@ -19,7 +17,6 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -32,19 +29,10 @@ class MainActivity : ComponentActivity() {
                     AppTheme.Auto -> isSystemInDarkTheme()
                 }
             ) {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                ) {
-                    AuthenticatorApp()
-                }
+                AuthenticatorApp()
             }
         }
     }
-
-
 }
-//fun NavController.navigate(route: String){
-//    popBackStack(route = route, inclusive = true)
-//
-//}
+
 
