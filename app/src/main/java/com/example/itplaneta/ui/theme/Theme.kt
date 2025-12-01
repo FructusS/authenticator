051141d5b0df44475c81622ceed23d130637910d
FructusS <afanasev10056@gmail.com>
2025-12-01 23:55:04 +0300

    add new colors

--- app/src/main/java/com/example/itplaneta/ui/theme/Theme.kt
+++ app/src/main/java/com/example/itplaneta/ui/theme/Theme.kt
@@ -1,24 +1,38 @@
 package com.example.itplaneta.ui.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import com.example.itplaneta.ui.screens.settings.AppTheme
 
 @Composable
 fun AuthenticatorTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
+    appTheme: AppTheme,
+    content: @Composable (() -> Unit)
 ) {
-    val colorScheme = when {
-        darkTheme -> DarkColors  // From Color.kt (already Material 3)
-        else -> LightColors      // From Color.kt (already Material 3)
+
+    val darkTheme = when (appTheme) {
+        AppTheme.Auto -> isSystemInDarkTheme()
+        AppTheme.Dark -> true
+        AppTheme.Light -> false
     }
+
+    val context = LocalContext.current
+    val colorScheme =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            if (darkTheme) dynamicDarkColorScheme(context)
+            else dynamicLightColorScheme(context)
+        } else {
+            if (darkTheme) DarkColors else LightColors
+        }
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = AppTypography,  // Material 3 compatible
+        typography = AppTypography,
         content = content
     )
 }
